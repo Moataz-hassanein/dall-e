7813fd741efa2d89f003b6c51dd7e58967edb578
Moataz Hassanein <moataz.hassanein87@gmail.com>
2023-11-14 14:28:32 +0100

    server deploying

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("https://dall-e-hl4i.onrender.com/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://dall-e-c9rl.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
